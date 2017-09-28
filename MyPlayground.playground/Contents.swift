cb4a1864475094c1f5c1278298d0420345eb7d14
Simon Ljungberg <hi@iamsim.me>
2017-09-28 11:12:40 +0200

    cleanup and licens

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,24 @@
-//: Playground - noun: a place where people can play
+/**
+ Copyright (c) 2017 Simon Ljungberg
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
 import PlaygroundSupport
 import Foundation
@@ -53,9 +73,6 @@ public enum FeedbackAction: TableViewDescriptor {
         }
     }
 }
-
-public final class FeedbackViewController: StaticTableViewController<FeedbackAction> {}
-
 
 let vc = StaticTableViewController<FeedbackAction>(style: .grouped)
 vc.title = "Send Feedback"
